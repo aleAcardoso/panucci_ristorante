26588b19560b655362b0bc1cea73453c34f83a21
Alexandre Antonelli Cardoso <aacds@msn.com>
2024-07-03 11:23:54 -0300

    Entendendo as constraints (#2)

---
+++ lib/screens/drinks_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrinksMenu extends StatelessWidget {
+  const DrinksMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
