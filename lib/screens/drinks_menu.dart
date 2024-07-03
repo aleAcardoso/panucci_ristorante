bf625574260f7d96898fad876827d12880ed9093
xandelol <aacds@msn.com>
2024-07-03 11:46:18 -0300

    Montando lista de bebidas com GridView

--- lib/screens/drinks_menu.dart
+++ lib/screens/drinks_menu.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/drink_item.dart';
 
 class DrinksMenu extends StatelessWidget {
   const DrinksMenu({super.key});
 
+  final List items = drinks;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+      child: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0, top: 16.0),
+              child: Text(
+                "Bebidas",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 32,
+                ),
+              ),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return DrinkItem(
+                  imageURI: items[index]["image"],
+                  itemTitle: items[index]["name"],
+                  itemPrice: items[index]["price"],
+                );
+              },
+              childCount: items.length
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 158/194
+            ),
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 16.0,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
